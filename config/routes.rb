a65369be09b0a03877194df61ec9def98bc86b9f
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-05-02 09:16:13 +0000

    [+association:models]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  
   root 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  
+  resources :users,only: [:index,:show,:edit,:update]
+  
+  resources :books,only: [:index,:show,:edit,:create,:update,:destroy] do
+    resources :favorites,only: [:create,:destroy]
+    resources :book_comments,only: [:create,:destroy]
+  end
 end
