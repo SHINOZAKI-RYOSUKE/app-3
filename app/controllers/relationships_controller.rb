8b9d1776bc97241ddebc0c6a338f0ab9b0817805
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-05-03 15:03:39 +0000

    [+controller:searches_search+Usermodel:if]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,6 @@
 class RelationshipsController < ApplicationController
   def create
-    follow = current_user.active_relationships.build(follower_id: params[:user_id])
+    follow = current_user.active_relationships.build(follower_id: params[:user_id])#明日きく！(buildについて)
     follow.save
     redirect_back(fallback_location: root_path)
   end
