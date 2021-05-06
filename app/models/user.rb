cfbe4c36156e2e002ff8ea3d09a47ef3199f1135
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-05-06 14:51:04 +0000

    [Q1]

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
     # 今自分(引数のuser)がフォローしようとしているユーザー(レシーバー)がフォローされているユーザー(つまりpassive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる
     passive_relationships.find_by(following_id: user.id).present?
   end
-  
+  #ここは必ず聞くこと！
   
   
  
